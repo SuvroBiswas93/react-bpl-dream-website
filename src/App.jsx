02c44bb0f492e3b11340412d267b2618d9d5072d
Suvro Biswas <shuvo.249963@gmail.com>
2025-09-22 02:32:39 +0600

    Add loader and load available players data in console

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@
 import AvailablePlayers from './Components/AvailablePlayers/AvailablePlayers'
 import SelectedPlayers from './Components/SelectedPlayers/SelectedPlayers'
 import Navbar from './Components/Navbar/Navbar'
+import { Suspense } from 'react';
+
+const fetchPlayers = async ()=>{
+  const res = await fetch('/Players.json');
+  return res.json()
+}
 
 function App() {
-  
-
+  const playersPromise = fetchPlayers()
   return (
     <>
       
       <Navbar></Navbar>
-      <AvailablePlayers></AvailablePlayers>
-      <SelectedPlayers></SelectedPlayers>
+      <Suspense fallback={<h3 className='font-bold max-w-[1200px] mx-auto'>Players are Loading <span className="loading loading-dots loading-xl"></span></h3>}>
+        <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
+      </Suspense>
+      {/* <SelectedPlayers></SelectedPlayers> */}
     </>
   )
 }
--- src/Components/AvailablePlayers/AvailablePlayers.jsx
+++ src/Components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { use } from 'react';
 
-const AvailablePlayers = () => {
+const AvailablePlayers = ({playersPromise}) => {
+    const players = use(playersPromise)
+    console.log(players)
     return (
-        <div>
-            
+        <div className='max-w-[1200px] mx-auto my-8'>
+            <h1 className='font-extrabold text-2xl'>Available Players : </h1>
         </div>
     );
 };
